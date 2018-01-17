d13383c6343edb0165d818119d1aef9d3de6d7c0
Bruno Coelho <4brunu@gmail.com>
2018-01-17 12:48:18 +0000

    remove new lines

--- BridgecraftTest/ObjcSampleClass.h
+++ BridgecraftTest/ObjcSampleClass.h
@@ -11,21 +11,12 @@
 #import "ViewController.h"
 
 @interface ObjcSampleClass : NSObject
-
 @property (copy, readonly) NSArray<NSNumber *> * _Nonnull nonnullArray;
-
 @property (copy, readonly) NSArray<NSNumber *> * _Nullable nullableArray;
-
 @property (copy, readonly) UIViewController * _Nullable viewControllerGeneric;
-
 @property (copy, readonly) ViewController * _Nullable viewController;
-
 - (NSNumber * _Nonnull)example1FunctionThatReturnsNonnull:(NSString * _Nullable)nullableParameter;
-
 - (NSNumber * _Nonnull)example2FunctionThatReturnsNonnull:(NSString * _Nonnull)nonnullParameter;
-
 - (NSNumber * _Nullable)example3FunctionThatReturnsNullable:(NSString * _Nullable)nullableParameter;
-
 - (AppDelegate * _Nonnull)example4FunctionThatReturnsNonnull:(ViewController * _Nullable)nullableParameter;
-
 @end
