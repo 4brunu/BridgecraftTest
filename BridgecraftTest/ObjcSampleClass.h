d8c267d24f775f6960f1ecd33263926c6fecc2b9
Bruno Coelho <4brunu@gmail.com>
2018-01-18 16:48:57 +0000

    add comments to source file

--- BridgecraftTest/ObjcSampleClass.h
+++ BridgecraftTest/ObjcSampleClass.h
@@ -10,13 +10,20 @@
 #import "AppDelegate.h"
 #import "ViewController.h"
 
+//Comment class
 @interface ObjcSampleClass : NSObject
 @property (copy, readonly) NSArray<NSNumber *> * _Nonnull nonnullArray;
 @property (copy, readonly) NSArray<NSNumber *> * _Nullable nullableArray;
 @property (copy, readonly) UIViewController * _Nullable viewControllerGeneric;
 @property (copy, readonly) ViewController * _Nullable viewController;
+//Comment 1
 - (NSNumber * _Nonnull)example1FunctionThatReturnsNonnull:(NSString * _Nullable)nullableParameter;
+/* Comment 2 */
 - (NSNumber * _Nonnull)example2FunctionThatReturnsNonnull:(NSString * _Nonnull)nonnullParameter;
+/*
+ * Comment 3
+ */
 - (NSNumber * _Nullable)example3FunctionThatReturnsNullable:(NSString * _Nullable)nullableParameter;
+/**Comment 4 */
 - (AppDelegate * _Nonnull)example4FunctionThatReturnsNonnull:(ViewController * _Nullable)nullableParameter;
 @end
