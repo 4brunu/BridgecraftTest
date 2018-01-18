458b2cadec4ebc149840ca960279a976b61a62ba
Bruno Coelho <4brunu@gmail.com>
2018-01-18 16:52:50 +0000

    add script to run Bridgecraft

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # BridgecraftTest
 
 This is a sample project to debug https://github.com/lvsti/Bridgecraft/issues/5
+
+To run this `sh run_bridgecraft.sh` and check file bridgecraft.swift for the output.
---
+++ run_bridgecraft.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+Pods/Bridgecraft/bin/bridgecraft BridgecraftTest.xcodeproj BridgecraftTest --assume-nonnull --sdk iphonesimulator --destination "platform=iOS Simulator,name=iPhone X,OS=latest"
+# > bridgecraft.swift
